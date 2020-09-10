9b7a60a56cba3e03b47aef64d0d5aa837353eb4e
Kelly Hwong <dianhuangkan@gmail.com>
2020-09-10 13:07:47 +0800

    refine codes for PyTorch

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # kaggle_digit_recognizer
+
 kaggle_digit_recognizer
+
+## Train by LeNet-5 with PyTorch
+
+```python
+python ./main-lenet5.py
+```
+
+## Train by ResNet56v2 with TensorFlow
+
+```python
+python ./main-resnet.py --n=6
+```
--- data_utils.py
+++ data_utils.py
@@ -20,50 +20,6 @@ import matplotlib.pyplot as plt
 from utils import makedir_exist_ok
 
 
-def visualize_data():
-    """ Load Config """
-    with open('./config.json', 'r') as f:
-        CONFIG = json.load(f)
-    DATA_ROOT = CONFIG["DATA_ROOT"]
-    DATA_ROOT_MNIST = CONFIG["DATA_ROOT_MNIST"]
-    BATCH_SIZE = CONFIG["BATCH_SIZE"]
-    MODEL_DIR = CONFIG["MODEL_DIR"]
-    makedir_exist_ok(MODEL_DIR)
-
-    os.chdir(DATA_ROOT)
-    print(f"Change current work directory to: {DATA_ROOT}")
-
-    print("Step 1: Loading data...")
-    data_train = pd.read_csv("data/train.csv").values
-    data_train = shuffle(data_train)  # shuffle after loading train csv
-    data_test = pd.read_csv("data/test.csv").values
-    num_train = data_train.shape[0]
-
-    print("Step 2: Converting data...")
-
-    X = data_train[:, 1:].reshape(data_train.shape[0], 1, 28, 28)
-    X = X.astype(float)
-    X /= 255.0
-    X = torch.from_numpy(X)
-
-    y = data_train[:, 0]
-    y = y.astype(int)
-    y = torch.from_numpy(y)
-    y = y.view(data_train.shape[0], -1)
-
-    for i in range(100):
-        img = X[i]
-        label = y[i]
-        print(img)  # PIL.Image.Image
-        # print(img.mode)  # mode L
-        print(label)
-        img = np.asarray(img)
-        print(img.shape)
-        img = img.reshape(28, 28)
-        plt.imshow(img, cmap="gray")
-        plt.show()
-
-
 def train_val_mask(num_all=42000, train_val_split=0.8, seed=42):
     """
     Inputs:
